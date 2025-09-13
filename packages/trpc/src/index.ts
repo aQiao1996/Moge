6f7069d79e76514ae8d050a952204aef7dd06b50
胖虎 <1424883981@qq.com>
2025-09-13 17:22:02 +0800

    ✨ feat: 有了公共types包,不需要trpc公共包了

--- packages/trpc/src/index.ts
+++
@@ -1,7 +0,0 @@
-// 重新导出 trpc 相关工具和类型
-export { createTRPCReact } from '@trpc/react-query';
-export { createTRPCClient, httpBatchLink } from '@trpc/client';
-
-// 为了避免复杂的类型问题，我们使用类型占位符
-// 实际的类型会在运行时由 tRPC 自动推断
-export type AppRouter = any;
