9f106fe17699f08bc8e7d6cd2993b39617e188ab
胖虎 <1424883981@qq.com>
2025-09-13 21:13:52 +0800

    ✨ feat: 删除无用代码

--- apps/frontend/src/lib/api/index.ts
+++ apps/frontend/src/lib/api/index.ts
@@ -6,14 +6,6 @@
 // 导出类型定义
 export * from './types';
 
-// 导出 API 客户端工具
-export * from './client';
-
 // 导出各个业务模块的 API
-export * from './auth';
-
-// 后续可以添加更多模块，例如：
+// export * from './auth';
 // export * from './user';
-// export * from './article';
-// export * from './upload';
-// export * from './notification';
